bd763b86c1b9dba875e311f67997b0aa5cdf76d5
TriuLeo <dangtrieu.lb@gmail.com>
2024-04-03 08:23:47 +0700

    them class DBConnection

---
+++ WinformProject/DBConnection.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Controls;
+using System.Windows;
+
+namespace WinformProject
+{
+    public class DBConnection
+    {
+        public void ChuyenSangDrive()
+        {
+            var prs = new ProcessStartInfo("\"C:\\Program Files (x86)\\CocCoc\\Browser\\Application\\browser.exe\"");
+            prs.Arguments = "https://drive.google.com/drive/home";
+            Process.Start(prs);
+        }
+        
+    }
+}
